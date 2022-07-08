0199f6c6d4f2ea2f66023684d91b049b8488fd4f
StasTr87 <stastr87@gmail.com>
2022-07-09 00:21:02 +0400

    добавил модуль пользователей

---
+++ users_module.py
@@ -0,0 +1,29 @@
+import logging
+import shelve
+import json
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s_%(levelname)s: %(message)s')
+
+class User_obj(object):
+    def __init__(self, user_name=None, user_id=None):
+        self.user_name = user_name
+        self.user_id = user_id
+
+    def __repr__(self):
+        user={}
+        user.update({'user_name':self.user_name})
+        user.update({'user_id':self.user_id})
+        return json.dumps(user)
+
+    def add_user_to_db(self):
+        with shelve.open('db',flag="c") as shelFile:
+            user_list=shelFile['users']
+            user_list.append(self)
+            shelFile['users']=user_list
+
+
+def load_users_from_db():
+    with shelve.open('db',flag="r") as shelFile:
+        user_list=shelFile['users']
+    return user_list
+
